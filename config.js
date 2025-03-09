d15f5f8957aaaa0cdeb11dac6ef3593212d92484
Dickens2007 <mwakijolodickens7@gmail.com>
2025-03-09 22:40:39 +0300

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ function convertToBool(text, fault = 'true') {
 
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID,
-GITHUB_USER_NAME: process.env.GITHUB_USER_NAME === undefined ? '' : process.env.GITHUB_USER_NAME,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'HANSAMAL-MD=MpogyaAQ#X2rO41Mdu7YW6iMPYImNCr2nwHw7mkAfDb5O206Y3PE' : process.env.SESSION_ID,
+GITHUB_USER_NAME: process.env.GITHUB_USER_NAME === undefined ? 'dickens2007' : process.env.GITHUB_USER_NAME,
 GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? '' : process.env.GITHUB_AUTH_TOKEN,        
-OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94711262551' : process.env.OWNER_NUMBER
+OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '255695 748 562' : process.env.OWNER_NUMBER
 };
